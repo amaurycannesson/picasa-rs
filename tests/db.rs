8ab65f840a3a2e2f9038ad54bfdf7ed7baf6550b
Amaury Cannesson <amaury.cannesson@gmail.com>
2025-06-27 08:39:16 +0800

    test: add semantic search integration tests

---
+++ tests/db.rs
@@ -0,0 +1,23 @@
+use std::sync::Once;
+
+use diesel::RunQueryDsl;
+use picasa_rs::database::{self, DbPool, schema};
+
+static INIT: Once = Once::new();
+
+pub fn get_pool() -> DbPool {
+    dotenvy::from_filename(".env.test").ok();
+
+    let mut pool = database::create_pool();
+    let mut conn = pool.get().unwrap();
+
+    INIT.call_once(|| {
+        database::run_migrations(&mut pool);
+    });
+
+    diesel::delete(schema::photos::table)
+        .execute(&mut conn)
+        .expect("Failed to clean test data");
+
+    pool
+}
---
+++ tests/semantic_search.rs
@@ -0,0 +1,33 @@
+use std::fs;
+
+use anyhow::Context;
+use diesel::connection::SimpleConnection;
+use picasa_rs::{
+    photo_repository::PgPhotoRepository,
+    services::{embedders::text::ClipTextEmbedder, semantic_search::SemanticSearchService},
+};
+use serial_test::serial;
+
+mod db;
+use db::get_pool;
+
+#[test]
+#[serial]
+fn test_should_find_photo_with_desk() {
+    let pool = get_pool();
+    let mut conn = pool.get().unwrap();
+
+    let sql =
+        fs::read_to_string("tests/data/fixtures/photos.sql").expect("Failed to read SQL file");
+    conn.batch_execute(&sql)
+        .expect("Failed to execute SQL script");
+
+    let photo_repository = PgPhotoRepository::new(pool);
+    let text_embedder = ClipTextEmbedder::new().expect("Failed to create embedder");
+    let mut semantic_search_service = SemanticSearchService::new(photo_repository, text_embedder);
+    let results = semantic_search_service
+        .search("desk", None, None)
+        .expect("Search failed");
+
+    assert_eq!(results[0].path, "data/images/sub/desk_vietnam.heic");
+}
